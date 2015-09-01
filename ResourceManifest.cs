6ff72b83b8e8652694c35189fabb0e5a59b93ebe
Kegan Maher <kegan.maher@smgov.net>
2015-09-01 14:29:29 -0700

    resource manifest to normalize script includes

---
+++ ResourceManifest.cs
@@ -0,0 +1,17 @@
+﻿using Orchard.UI.Resources;
+
+namespace CSM.BootstrapBase
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+
+            manifest
+                .DefineScript("Bootstrap")
+                .SetUrl("bootstrap/dist/bootstrap.min.js", "bootstrap/dist/bootstrap.js")
+                .SetDependencies("jQuery");
+        }
+    }
+}
