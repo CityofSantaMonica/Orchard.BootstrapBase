121fca80c619895ef66fb332afe73c5a1e2ff290
Kegan Maher <kegan.maher@smgov.net>
2016-05-03 11:18:10 -0700

    moving some logic to a class file

---
+++ LayoutHelpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace CSM.BootstrapBase
+{
+    public class LayoutHelpers
+    {
+        public static string CalcuClassify(dynamic model, string[] zoneNames, string classNamePrefix)
+        {
+            var zoneCounter = 0;
+            var zoneNumsFilled = string.Join("", zoneNames.Select(zoneName => { ++zoneCounter; return model[zoneName] != null ? zoneCounter.ToString() : ""; }).ToArray());
+            return HasText(zoneNumsFilled) ? classNamePrefix + zoneNumsFilled : "";
+        }
+
+        public static bool HasItems(dynamic model)
+        {
+            return model != null && model.Items != null && model.Items.Count > 0;
+        }
+
+        public static bool HasText(object thing)
+        {
+            return !String.IsNullOrWhiteSpace(Convert.ToString(thing));
+        }
+    }
+}
