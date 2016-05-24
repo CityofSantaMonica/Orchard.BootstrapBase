2f53d503c3523ed49694392b73584b35bc64d84f
Kegan Maher <kegan.maher@smgov.net>
2016-05-24 14:05:04 -0700

    parameterized element snippet to enable collapsible content sections

---
+++ ResourceManifest.cs
@@ -0,0 +1,17 @@
+﻿using Orchard.UI.Resources;
+
+namespace CSM.BootstrapBase
+{
+    public class ResourceManifest : IResourceManifestProvider
+    {
+        public void BuildManifests(ResourceManifestBuilder builder)
+        {
+            var manifest = builder.Add();
+
+            manifest
+                .DefineScript("CollapseContent")
+                .SetUrl("CollapseContent/collapse-content.js")
+                .SetDependencies("JQuery");
+        }
+    }
+}
---
+++ Scripts/CollapseContent/collapse-content.js
@@ -0,0 +1,31 @@
+﻿function collapseContent(rootSelector, highLevel, lowLevel) {
+    var slugs = {};
+    $(rootSelector).each(function (containerIndex) {
+        var parent = $(this);
+        for (level = lowLevel; level >= highLevel; level--) {
+            var targetHeading = parent.find("h" + level + ":not(:has(>a))").each(function (index) {
+                var heading = $(this);
+                //create the unique slug for this heading
+                var slug = "h" + level + "-" + heading.text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '');
+                if (slug in slugs) {
+                    slugs[slug] = slugs[slug] + 1;
+                    slug = slug + "_" + slugs[slug];
+                }
+                else {
+                    slugs[slug] = 1;
+                }
+                //apply attributes required for collapsible
+                heading.attr({ "aria-expanded": "false", "aria-controls": slug, "data-toggle": "collapse", href: "#" + slug });
+                //determine child content
+                var breakingHeaders = "h" + level;
+                for (sublevel = level - 1; sublevel >= 1; sublevel--) {
+                    breakingHeaders += ",h" + sublevel;
+                }
+                //wrap child content in collapsible container
+                var downstream = heading.nextUntil(breakingHeaders);
+                var panel = $("<div></div>").attr({ id: slug, "class": "collapse" });
+                downstream.wrapAll(panel);
+            });
+        }
+    });
+}
